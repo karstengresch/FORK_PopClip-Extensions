e5a2427afa49945263156d3ffa68cce0f55633b3
Karsten Gresch <git-2015@by-doing.org>
2024-10-01 19:25:58 +0200

    Reverse String impl

--- source/ReverseString.popclipext/Config.js
+++ source/ReverseString.popclipext/Config.js
@@ -4,5 +4,6 @@
 // description: Reverses the selected string
 // icon: square filled RS
 // popclip version: 4615
+// language: javascript
 
   return popclip.input.txt.split('').reverse().join('');
